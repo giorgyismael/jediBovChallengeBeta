eff18dda6571c589b923459450dc1f8b0423f062
giorgyismael <giorgyismael@gmail.com>
2016-10-20 15:14:45 -0200

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ angular.module("jetForce").config(function (BackandProvider) {
 })
 
 angular.module("jetForce").factory('token', function() {
-    var token = "Bearer wgLJM4aVW3iIl-DdDBlfEjfYMuD9bvZgZtg-kNDJMSjDGNEVLIHImvymyHz41YAMqC8vAlg6E4g4tF1ZERAt1DtG2kB3j63-iL2ZaVfhKMEVlXXfuBmLidob1rlfKgsX-Nja7hoiwvltmmmGQxHn-v0BEUBzdOvOU5tBKNQSuYbVdbCzxStEwOqIuic4k1eUh-griNr99X6Bq-NVoX319iC_olfsXLb4Rb3_g-IoMnJOmAy_TQpPZzzpgv9mhRFsjRMCy_ivYXW6jUPtqtKNYQ"
+    var token = "Bearer wgLJM4aVW3iIl-DdDBlfEjfYMuD9bvZgZtg-kNDJMSjDGNEVLIHImvymyHz41YAMqC8vAlg6E4g4tF1ZERAt1DtG2kB3j63-iL2ZaVfhKMEVlXXfuBmLidob1rlfKgsX-Nja7hoiwvltmmmGQxHn-v0BEUBzdOvOU5tBKNQSuYbVdbCzxStEwOqIuic4k1eUh-griNr99X6Bq-NVoX319iC_olfsXLb4Rb3_g-IoMnJOmAy_TQpPZzzpgv9mhRFsjRMCy_ivYXW6jUPtqtKNYQ";
       getValue: function() {
         return token;
       },
